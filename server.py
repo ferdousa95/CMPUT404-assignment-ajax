600cc728bb101d223ab5ebcd576fba65e34f3159
Ferdous Adit <ferdousa@ualberta.ca>
2023-03-08 17:58:49 -0700

    updated the server file

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 import json
 app = Flask(__name__)
 app.debug = True
@@ -74,27 +74,35 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", code=302)
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    myWorld.set(entity, data)
+    obj = json.dumps(myWorld.get(entity))
+    return obj
+    
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    obj = json.dumps(myWorld.world())
+    return obj
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    obj = json.dumps(myWorld.get(entity))
+    return obj
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    obj = json.dumps(myWorld.world())
+    return obj
 
 if __name__ == "__main__":
     app.run()
